42af75cfd7c3340b729fa04acf410670afb4254d
Jonathan Jang <62774081+JonathanJang24@users.noreply.github.com>
2021-07-08 23:53:41 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,9 +2,17 @@ import tkinter as tk
 import os
 from xml.etree import ElementTree
 
+# Testing split and scraping data from text files
 file_name = open('data\Address.txt')
 for r in file_name:
-    print(r)
+    r = r.split('\t')
+    id = r[1]
+    address = ' '.join(r[2:9])
+    coords = ' '.join(r[10:12])
+    print('id: ' + id)
+    print('address: ' + address)
+    print('coordinates: ' + coords)
+    break
 
 
 def analyze(address, zipcode, sex, age):
